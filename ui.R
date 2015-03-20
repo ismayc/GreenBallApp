0743b5090269c70f4cd39c418f018f95cd94ff44
Chester Ivan Ismay <chester.ismay@gmail.com>
2015-03-20 15:41:12 -0500

    Updated to 400 draws

--- ui.R
+++ ui.R
@@ -1,19 +1,16 @@
 library(shiny)
 
-# Define UI for application that plots random distributions 
 shinyUI(fluidPage(
-  #pageWithSidebar(
   
   # Application title
   headerPanel(""),
   
-  # Show a plot of the generated distribution
   mainPanel(
     withMathJax(),
     numericInput("draws", 
                  "Number of draws:", 
                  min = 1,
-                 max = 257, 
+                 max = 400, 
                  value = 1),
     plotOutput("distPlot"),
     uiOutput("mean"),
